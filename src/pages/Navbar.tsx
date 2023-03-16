54277040112ac4122c0352494b9d8de9f0191125
angularbrackets <mohammad@angularbrackets.com>
2023-03-15 23:06:28 -0600

    hide mobile menu on mouse leave

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -131,17 +131,17 @@ const Navbar = () => {
       {/* To: "opacity-0 scale-95" */}
       <div ref={mobileMenuRef} className={`mobile-menu ${showMobileMenu ? 'block' : 'hidden'} `} onMouseLeave={() => setShowMobileMenu(false)} >
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <Link href="#Home" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+          <Link href="#Home" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => setShowMobileMenu(false)}>
             
               Home
             
           </Link>
-          <Link href="#PrayerTimes" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+          <Link href="#PrayerTimes" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => setShowMobileMenu(false)}>
             
               Prayer Times
             
           </Link>
-          <Link href="#Programs" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+          <Link href="#Programs" className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium" onClick={() => setShowMobileMenu(false)}>
             
               Programs
             
